10e348c77e6e4d7ae628aba226675db8d3ee5453
4ddev <4ddevops@gmail.com>
2020-07-19 14:40:40 +0200

    Session

--- Communication/Backend/SessionController.js
+++ Communication/Backend/SessionController.js
@@ -1,6 +1,32 @@
 class SessionController {
     constructor(db){
+        if ( db == null ) throw Error("No Database given");
         this.db = db;
+    }
+
+    // Stackoverflow https://stackoverflow.com/questions/3393854/get-and-set
+    //  -a-single-cookie-with-node-js-http-server
+    parseCookies(request) {
+        var list = {},
+        rc = request.headers.cookie;
+    
+        rc && rc.split(';').forEach(function( cookie ) {
+            var parts = cookie.split('=');
+            list[parts.shift().trim()] = decodeURI(parts.join('='));
+        });
+        return list;
+    }
+
+    readSessionID( request ){
+        console.log( "TEST" );
+    }
+
+    createSessionID(){
+        // To Write a Cookie
+        response.writeHead(200, {
+            'Set-Cookie': 'mycookie=test',
+            'Content-Type': 'text/plain'
+        });
     }
 }
 
